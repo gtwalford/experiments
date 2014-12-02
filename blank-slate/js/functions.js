066dd9ad29cd6428b591230cd053cb41d4f28989
Gabriel Walford <gwalford@cpbgroup.com>
2014-12-02 09:11:44 -0700

    Apparently renamed things

---
+++ blank-slate/js/functions.js
@@ -0,0 +1,8 @@
+// SVG Line Animation
+// Based on work from Jake Archibald - http://jakearchibald.com/2013/animated-line-drawing-svg/
+
+(function($){
+  $(document).ready(function (){
+
+  });
+})(window.jQuery);
---
+++ svg-clip/README.md
@@ -0,0 +1,4 @@
+SVG Line Animation
+------------------
+An Experiment doing SVG line animations on click events.
+Based on JS from Jake Archibald - http://jakearchibald.com/2013/animated-line-drawing-svg/
